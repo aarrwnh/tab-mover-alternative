ffa854ca1fa7746d340e5e9da18985afe8edf1e7
Wenereth Arkhilmor <7783383+aarrwnh@users.noreply.github.com>
2022-09-27 02:36:08 +0900

    fix(downloads): set event listener before actual download

--- src/background/src/browser/Download.ts
+++ src/background/src/browser/Download.ts
@@ -2,6 +2,24 @@ import { convertFullWidthToHalf } from "../utils/characterShift";
 import { replaceIllegalCharacters } from "../utils/replaceIllegalCharacters";
 import { Notifications } from "./Notifications";
 
+function encodeFilename(opts: browser.downloads._DownloadOptions) {
+	// hack/bug?: even having unicode char before slash will error when saving,
+	// so just save as encoded URI, and then decode manually after save
+	// with eg. python's `urllib.parse.unquote`
+	opts.filename = opts.filename
+		?.split("/")
+		.map((x, i, arr) => {
+			// skip on filename, just encode folders
+			return i !== arr.length - 1 ? encodeURIComponent(x) : x;
+		})
+		.join("/");
+}
+
+function tryUnderlineFilename(opts: browser.downloads._DownloadOptions) {
+	opts.filename = opts.filename!.replace(/([^\x00-\x7F]{1})\//g, "$1_/");
+}
+
+
 export class Downloads extends Notifications {
 	private _eraseAfterComplete?: boolean;
 
@@ -26,66 +44,17 @@ export class Downloads extends Notifications {
 			}
 		}
 
-		// add some cooldown between each download because it gets stuck
-		// at weird point
-		await new Promise((r) => setTimeout(r, 50)); // 30 -- too low
-
-		let downloadID = -1;
-		try {
-			downloadID = await browser.downloads.download(opts);
-		}
-		catch (err) {
-			console.error(err);
-			if (err instanceof Error) {
-				const isInvalidFilename = err.message.includes("illegal characters");
-				if (isInvalidFilename) {
-					if (opts.filename?.match(/[^\x00-\x7F]{1}\//)) {
-						// try to add underline first
-						opts.filename = opts.filename.replace(/([^\x00-\x7F]{1})\//g, "$1_/");
-						return this.start(opts, true);
-					}
-					else {
-						// hack/bug?: even having unicode char before slash will error when saving,
-						// so just save as encoded URI, and then decode manually after save
-						// with eg. python's `urllib.parse.unquote`
-						opts.filename = opts.filename
-							?.split("/")
-							.map((x, i, arr) => {
-								// skip on filename, just encode folders
-								return i !== arr.length - 1 ? encodeURIComponent(x) : x;
-							})
-							.join("/");
-						return this.start(opts, true);
-					}
-				}
-				throw new Error(err.message + ": " + opts.url);
-			}
-		}
-
-		const downloadItem = await browser.downloads.search({ id: downloadID });
-		if (downloadItem.length > 0 && downloadItem[0].state === "complete") {
-			browser.downloads.erase({ id: downloadID });
-			return Promise.resolve();
-		}
-
-		return await this._onDownloadStateChange();
-	}
-
-	private async _onDownloadStateChange(): Promise<void> {
-		let removeAfterComplete = this._eraseAfterComplete;
-
-		return new Promise<void>(function (resolve, reject) {
-			function erase(id: number): void {
-				if (removeAfterComplete) {
+		return new Promise<void>((resolve, reject) => {
+			const erase = (id: number): void => {
+				if (this._eraseAfterComplete) {
 					browser.downloads.erase({ id });
 				}
 				browser.downloads.onChanged.removeListener(onDownloadEnd);
-			}
+			};
 
 			function onDownloadEnd(delta: browser.downloads._OnChangedDownloadDelta): void {
 				if (delta.state) {
 					if (delta.state.current === "interrupted") {
-						removeAfterComplete = false;
 						reject(delta.error);
 					}
 					if (delta.state.current === "complete") {
@@ -96,6 +65,33 @@ export class Downloads extends Notifications {
 			}
 
 			browser.downloads.onChanged.addListener(onDownloadEnd);
+
+			browser.downloads.download(opts)
+				.catch((err: Error) => {
+					console.error(err);
+
+					if (err instanceof Error) {
+						const isInvalidFilename = err.message.includes("illegal characters");
+
+						if (isInvalidFilename) {
+							if (opts.filename?.match(/[^\x00-\x7F]{1}\//)) {
+								tryUnderlineFilename(opts);
+							}
+							else {
+								encodeFilename(opts);
+							}
+
+							resolve(this.start(opts, true));
+
+							return;
+						}
+						else {
+							throw new Error(err.message + ": " + opts.url);
+						}
+					}
+
+					reject("unknown error");
+				});
 		});
 	}
 }
