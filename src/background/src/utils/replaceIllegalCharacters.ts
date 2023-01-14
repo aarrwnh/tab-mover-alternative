e74f90b85b3e3f4dd5521d8079eb297615108664
Wenereth Arkhilmor <7783383+aarrwnh@users.noreply.github.com>
2023-01-14 21:29:08 +0900

    fix: remove additional unicode glyphs in filename

--- src/background/src/utils/replaceIllegalCharacters.ts
+++ src/background/src/utils/replaceIllegalCharacters.ts
@@ -11,13 +11,18 @@ const ILLEGAL_SYSTEM_CHAR: {
 	"?": "\uff1f", // ？
 	"\\": "\uff3c", // ＼
 	"\u3000": "\x20", // full-width space
+	"\u200d": "",
 };
 
+const emoji = /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g;
 const escapedChars = Object.keys(ILLEGAL_SYSTEM_CHAR).map((x) => "\\" + x).join("");
 const RE_ILLEGAL = new RegExp("[" + escapedChars + "]", "g");
 
 export function replaceIllegalCharacters(str: string): string {
-	return str.replace(RE_ILLEGAL, function (m) {
-		return ILLEGAL_SYSTEM_CHAR[m];
-	});
+	return str
+		.replace(RE_ILLEGAL, function (m) {
+			return ILLEGAL_SYSTEM_CHAR[m];
+		})
+		.replaceAll(emoji, "")
+		.trim();
 }
